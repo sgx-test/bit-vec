2efa94d52a79dbfde199abbf73ca7af30bcd7565
prz23 <panruizheng@zju.edu.cn>
2021-07-30 13:37:48 +0800

    support sgx

--- src/lib.rs
+++ src/lib.rs
@@ -83,6 +83,13 @@
 #![doc(html_root_url = "https://docs.rs/bit-vec/0.6.3")]
 
 #![no_std]
+#![cfg_attr(all(feature = "mesalock_sgx", not(target_env = "sgx")), no_std)]
+#![cfg_attr(all(target_env = "sgx", target_vendor = "mesalock"), feature(rustc_private))]
+
+#[cfg(all(feature = "mesalock_sgx", not(target_env = "sgx")))]
+#[macro_use]
+extern crate sgx_tstd as std;
+use std::prelude::v1::*;
 
 #[cfg(any(test, feature = "std"))]
 #[macro_use]
